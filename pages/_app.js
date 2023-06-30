77e18037dae88b90625affa403188eb29b9b7a77
Ling Fung Chan <justinlfchan@gmail.com>
2023-06-30 16:27:23 +0100

    updated font-family to PlayFair Display

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Layout } from "./components";
 
 import "@/styles/globals.css";
+// import { Inter } from "@next/font/google";
 
 export default function App({ Component, pageProps }) {
   return (
