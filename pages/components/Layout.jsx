c491778af8bfcedfb02df4aff61dc07475125d26
Ling Fung Chan <justinlfchan@gmail.com>
2023-06-30 15:35:29 +0100

    imported home.jsx as a component in layout.jsx <main /> and loaded layout.jsx in _app.js

--- pages/components/Layout.jsx
+++ pages/components/Layout.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Layout = () => {
-  return <div>Layout</div>;
+// This "head" refers to the HTML <head /> and all those metadata
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>THY Perfumery - by Ling Fung Chan</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
