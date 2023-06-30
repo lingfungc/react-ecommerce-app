29fa032006742cd4c1bd4a2e5d343aead3f3254c
Ling Fung Chan <justinlfchan@gmail.com>
2023-06-30 22:11:29 +0100

    added product-detail content and updated product-detail css styles

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -2,6 +2,13 @@ import React from "react";
 
 import { client, urlFor } from "../../lib/client";
 
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
+
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
 
@@ -12,23 +19,49 @@ const ProductDetails = ({ product, products }) => {
           <div className="image-container">
             <img src={urlFor(image && image[0])} />
           </div>
+          {/* <div className="small-images-container"> */}
+          {/* This "?" is to ensure the "image" array exists */}
+          {/* {image?.map((item, i) => (
+              <img src={urlFor(item)} className="" onMouseEnter="" />
+            ))} */}
+          {/* </div> */}
+        </div>
+
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            {/* <div> */}
+            {<AiFillStar />}
+            {<AiFillStar />}
+            {<AiFillStar />}
+            {<AiFillStar />}
+            {<AiOutlineStar />}
+            {/* </div> */}
+            <p>(20)</p>
+          </div>
+          <div>
+            <h4>Details: </h4>
+            <p>{details}</p>
+            <p className="price">£{price}</p>
+          </div>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick="">
+                <AiOutlineMinus />
+              </span>
+              <span className="num" onClick="">
+                0
+              </span>
+              <span className="plus" onClick="">
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
   );
-};
-
-export const getStaticPaths = async () => {
-  return {
-    paths: [
-      {
-        params: {
-          slug: "'${slug}'.js",
-        },
-      },
-    ],
-    fallback: "blocking",
-  };
 };
 
 // * This block of code is from JavaScript Mastery, but not working
@@ -50,6 +83,19 @@ export const getStaticPaths = async () => {
 //     fallback: "blocking",
 //   };
 // };
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: {
+          slug: "'${slug}'.js",
+        },
+      },
+    ],
+    fallback: "blocking",
+  };
+};
 
 export const getStaticProps = async ({ params: { slug } }) => {
   const product = await client.fetch(
