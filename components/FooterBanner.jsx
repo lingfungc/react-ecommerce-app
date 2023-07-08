22e60cdfd0a9044c8477d13e50f4e6980cf40eea
Ling Fung Chan <justinlfchan@gmail.com>
2023-07-08 21:20:14 +0100

    redoing last commit

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -30,7 +30,7 @@ const FooterBanner = ({
           <p>{smallText}</p>
           <h3>{midText}</h3>
           <p>{desc}</p>
-          <Link href={`/product/${product}}`}>
+          <Link href={`/products/${slug.current}`}>
             <button type="button">{buttonText}</button>
           </Link>
         </div>
